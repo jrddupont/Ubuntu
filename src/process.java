9e38a5a6c296a5c7fb5dc2eaa8c5bf163e5166cf
Schneider <hh7795rk@winona.edu>
2017-10-30 14:48:35 -0500

    made better

---
+++ src/process.java
@@ -0,0 +1,14 @@
+
+public class process {
+	int id;
+	int ioTime;
+	int cpuTime;
+	int memory;
+	
+	process( int id, int ioTime, int cpuTime, int memory ){
+		this.id = id;
+		this.ioTime = ioTime;
+		this.cpuTime = cpuTime;
+		this.memory = memory;
+	}
+}
