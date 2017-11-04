3708764fbbde86976d65dfaf389e0d8897cbb10a
doominabox1 <jrddupont@gmail.com>
2017-11-04 15:41:29 -0500

    Added proccess table

---
+++ src/Driver.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+import bursts.Burst;
+import bursts.CPUBurst;
+import bursts.IOBurst;
+
+
+public class Driver {
+	public static void main(String[] args){
+		ArrayList<Burst> p1Bursts = new ArrayList<Burst>();
+		p1Bursts.add(new CPUBurst(3));
+		p1Bursts.add(new IOBurst(14));
+		p1Bursts.add(new CPUBurst(3, 2, 2));
+		
+		ArrayList<Burst> p2Bursts = new ArrayList<Burst>();
+		p2Bursts.add(new CPUBurst(5));
+		p2Bursts.add(new IOBurst(7));
+		p2Bursts.add(new CPUBurst(1, 2, 1));
+		
+		ArrayList<Burst> p3Bursts = new ArrayList<Burst>();
+		p3Bursts.add(new CPUBurst(6));
+		
+		Process[] processes = {
+				new Process(1, 64, p1Bursts),
+				new Process(2, 32, p2Bursts),
+				new Process(3, 32, p3Bursts),
+		};
+	}
+}
+
--- src/bursts/CPUBurst.java
+++ src/bursts/CPUBurst.java
@@ -1,5 +1,18 @@
 package bursts;
 
 public class CPUBurst extends Burst{
-
+	public boolean hasCS;
+	public int startBurst;
+	public int csBurst;
+	public int endBurst;
+	public CPUBurst(int start, int cs, int end){
+		startBurst = start;
+		csBurst = cs;
+		endBurst = end;
+		hasCS = true;
+	}
+	public CPUBurst(int burst){
+		startBurst = burst;
+		hasCS = false;
+	}
 }
