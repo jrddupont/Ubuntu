208d8fd4fd5b41fb38a2e14faff183bc7604f597
Schneider <hh7795rk@winona.edu>
2017-11-04 17:29:25 -0500

    Added age Changed scheduling

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.TreeMap;
 
 
@@ -6,25 +7,43 @@ public class Scheduler {
 	
 	Process[] processes;
 	
-	MemoryManager memory = new MemoryManager();
-	
 	static TreeMap<Long, ArrayList<Process>> schedulingQueue = new TreeMap<Long, ArrayList<Process>>();
 
 	Scheduler( Process[] processes ){
 		this.processes = processes;
 		
-		Process a = new Process(1, 0, 0, 0  );
-		Process b = new Process(2, 0, 0, 0  );
+		Process a = new Process(1, 0, new ArrayList() );
+		Process b = new Process(2, 0, new ArrayList() );
 		
+		getNextProcess();
 		
+		storeProcess( a, 0 );
+		storeProcess( b, 0 );
 		
-		StoreProcess( a, 0 );
-		StoreProcess( b, 100 );
-		
-		System.out.println( schedulingQueue.get( schedulingQueue.firstKey() ).get(1).id );
+		System.out.println( schedulingQueue.get( schedulingQueue.firstKey() ).get(0).id );
 	}
 	
-	private void StoreProcess( Process process, long executionTime ){
+	
+	
+	//Get the next process to be run aka the one with lowest execution time
+	private Process getNextProcess(){
+		try{
+			return schedulingQueue.get( schedulingQueue.firstKey() ).get( 0 );
+		} catch( NoSuchElementException e ){
+			return null;
+		}
+	}
+	
+	//Remove process from schedulingQueue
+	private void removeProcess( Process process ){
+		
+		//Tell memory it's gone
+		MemoryManager.deallocate( process );
+		
+	}
+	
+	//Store a process in the schedulingQueue
+	private void storeProcess( Process process, long executionTime ){
 		
 		ArrayList<Process> list;
 		
