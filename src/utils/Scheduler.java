357fbeef3cd369522d161bb4fd68ed20b340b2f5
John <jschneider14@winona.edu>
2017-11-30 10:07:19 -0600

    now works, as far as I can tell.  It tries to remove items from memory a bunch of times in a row sometimes but who knows if that's a problem.  Needs print statements e'erwhere

--- src/utils/Scheduler.java
+++ src/utils/Scheduler.java
@@ -46,9 +46,6 @@ public class Scheduler {
 			//Also get the current process' TimePair
 			TimePair currentTimePair = schedulingQueue.first();
 			
-			//Remove the process from the scheduling queue
-			schedulingQueue.pollFirst();
-			
 			//AAAAAAAAAAAAAAAAHHHHHHHHHHH
 			if( currentProcess.isDone() ){
 				continue;
@@ -59,6 +56,7 @@ public class Scheduler {
 			
 			System.out.println( "Process #" + currentProcess.id + " has started running with runtime of " + currentProcess.getRuntime() + " out of a total " + currentProcess.getEstimatedTotalRuntime() + " runtime" );
 			System.out.println( "Process #" + currentProcess.id + " has timeslice " + timeSlice );
+			System.out.println( "There are currently " + schedulingQueue.size() + " items in the scheduling queue" );
 			//Execute this process' time slice
 			for( int i = 0; i < timeSlice; i++ ){
 				
@@ -75,37 +73,47 @@ public class Scheduler {
 					continue outerExecution;
 				}
 				
-				//After we've finished stepping, check if we just finished
+				//After we've finished stepping, check if we just finished so we can stop trying to step.
 				if( currentProcess.isDone() ){
 					System.out.println( "Process #" + currentProcess.id + " is finished" );
-					//If we've finished, we need to deallocate this 
 					
-					MemoryManager.deallocate( currentProcess );
-					
-					//Continue onto the next process in the queue
-					continue outerExecution;
-					
-				}else{
-					
-					//Update process' TimePair's Virtual Runtime
-					currentTimePair.virtualRuntime = getVirtualRuntime( currentProcess );
-					
-					System.out.println( "Process #" + currentProcess.id + " has loaded into the scheduling queue" );
-					
-					//Insert TimePair into the scheduling queue
-					schedulingQueue.add( currentTimePair );
+					//Break out of the timeslice for loop
+					break;
 				}
+			}
+			
+			//Remove the process from the scheduling queue
+			//We're doing it here so that nice values are handled properly.
+			schedulingQueue.pollFirst();
+			
+			if ( currentProcess.isDone() ){
+				
+				//If we've finished, we need to deallocate this 
+				MemoryManager.deallocate( currentProcess );
+				
+			}else{
+				
+				//If the process is still "good" we should update the virtual runtime and add it back into the tree.
+				
+				//Update process' TimePair's Virtual Runtime
+				currentTimePair.virtualRuntime = getVirtualRuntime( currentProcess );
+				
+				//Insert TimePair into the scheduling queue
+				schedulingQueue.add( currentTimePair );
 				
 			}
+			
 		}
 	}
 	
 	private int getTimeSlice( Process process ){
 		
+		
+		
 		double nice = getNiceValue( process );
 		double totalNice = getTotalNice();
 		
-		System.out.println( "AAAAAAAAAAAAH " + (double) getPeriod() );
+		System.out.println( "Process #" + process.id + " has a nice value of " + nice + " and a totalNice of " + totalNice + " with a period of " + getPeriod() );
 		
 		double slice = getPeriod() * ( nice / totalNice );
 		
@@ -123,14 +131,14 @@ public class Scheduler {
 		
 		//Largest value in r/b is item with longest runtime
 		
-		double largestRuntime = schedulingQueue.last().virtualRuntime;
-		
-		//Avoid infinity
-		if( largestRuntime == 0 ){
-			return 0;
-		}
-		
-		double virtualRuntime = ( (double) process.getRuntime() / (double) largestRuntime  );
+//		double largestRuntime = schedulingQueue.last().virtualRuntime;
+//		
+//		//Avoid infinity
+//		if( largestRuntime == 0 ){
+//			return 0;
+//		}
+//		
+//		double virtualRuntime = ( (double) process.getRuntime() / (double) largestRuntime  );
 		
 		return process.getRuntime();
 		
@@ -139,7 +147,7 @@ public class Scheduler {
 	
 	
 	//"Nice values" indicate the priority of a process from -20 to 19 where lower is higher priority
-	//For our purposes here, I'll be doing a simple slide conversion from priority 1->-20, 2->-19, etc.
+	//For our purposes here, I'll be converting IDs to a value that can be easily made into a fraction later for time slice calculations
 	//Also of note is that process priority = process ID
 	private int getNiceValue( Process process ){
 		int maxID = 0;
@@ -149,14 +157,19 @@ public class Scheduler {
 			}
 		}
 		
-		return process.id - ( maxID + 1 );
+		double nice = process.id - ( maxID + 1 );
+		
+		return (int) Math.abs( nice );
 	}
 	
 	//Total of all nice values in the run queue
 	private double getTotalNice(){
 		double totalNice = 0;
 		for(TimePair tp : schedulingQueue){
-			totalNice += getNiceValue(tp.process);
+			
+			double nice = getNiceValue( tp.process );
+			
+			totalNice += nice;
 		}
 		return totalNice;
 	}
