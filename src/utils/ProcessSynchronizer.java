017a9d2f663a111ce6bc7a4e78d1d50f70806a33
doominabox1 <jrddupont@gmail.com>
2017-12-01 16:48:57 -0600

    Solved

--- src/utils/ProcessSynchronizer.java
+++ src/utils/ProcessSynchronizer.java
@@ -6,10 +6,6 @@ public class ProcessSynchronizer {
 	private static ArrayList<Integer> locks = new ArrayList<Integer>();
 	
 	public static boolean lock(Integer resource){
-		for(int i : locks){
-			System.out.print(i + " ");
-		}
-		System.out.println();
 		if(locks.contains(resource)){
 			return false;
 		}
